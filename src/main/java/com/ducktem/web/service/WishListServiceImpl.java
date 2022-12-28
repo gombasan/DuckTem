534c7dc9515b308d648e9eade87a2b5bb8b126fe
jihyeon <biggums0223@gmail.com>
2022-12-28 16:53:29 +0900

    찜 기능(메인, 디테일 화면) 구현 1차 완성본

--- src/main/java/com/ducktem/web/service/WishListServiceImpl.java
+++ src/main/java/com/ducktem/web/service/WishListServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ducktem.web.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +20,11 @@ public class WishListServiceImpl implements WishListService{
     private MemberDao memberDao;
     @Autowired
     private WishListDao wishListDao;
-	
+    
+//    my wishList 불러오기
     @Override
-    public void findWish(String userId, Long productId) {
-		
-		WishList wishList =  new WishList(userId, productId);
-		
-        wishListDao.get(wishList);
+    public List<WishList> findWish(String userId) {
+		return wishListDao.getList(userId);
 		
     }
     
@@ -32,7 +32,7 @@ public class WishListServiceImpl implements WishListService{
 	@Override
 	public void checkWish(String userId, Long productId) {	
 		
-		WishList wishList =  new WishList(userId, productId);
+		WishList wishList =  new WishList(productId,userId);
 		
         wishListDao.save(wishList);
 		
@@ -41,11 +41,29 @@ public class WishListServiceImpl implements WishListService{
 	@Override
 	public void deleteWish(String userId, Long productId) {
 		
-		WishList wishList =  new WishList(userId, productId);
+		WishList wishList =  new WishList(productId,userId);
 		
         wishListDao.delete(wishList);
 		
 		
+	}
+
+	@Override
+	public boolean confirmWish(String userId, Long productId) {
+		
+		WishList resultWishList = wishListDao.get(userId, productId);
+		
+		System.out.println(resultWishList);
+		if(resultWishList==null)
+			return false;
+		else
+			return true;
+	}
+
+	@Override
+	public int getWIshNums(Long productId) {
+		int nums = wishListDao.getNums(productId);
+		return nums;
 	}
 
 
