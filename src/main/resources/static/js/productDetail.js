5ca2f2b2100bf030ff90da882beb25d222980a04
miji7575 <mipark7575@gmail.com>
2023-01-06 10:16:00 +0900

    miji-detail css수정

--- src/main/resources/static/js/productDetail.js
+++ src/main/resources/static/js/productDetail.js
@@ -10,25 +10,25 @@ import wish from "./wish.js";
 //==========================================================
 window.addEventListener('load', function() {
 
-	let chatting = function() {
+//	let chatting = function() {
+//
+//
+//    let duckTalk = document.querySelector("#duck-talk");
+//    let query = duckTalk.dataset.id
+//    let sellerId = query.split("/").at(0);
+//    let customerId = query.split("/").at(1);
+//    let TempProductId = query.split("/").at(2);
+//    let productId = Number.parseInt(TempProductId);
+//    if(sellerId != "null"){
+//        duckTalk.onclick = function (e) {
+//            e.preventDefault();
+//            console.log("/chatting?sellerId="+sellerId+"&&customerId="+customerId+"&&productId="+productId);
+//          window.location.href = "/chatting?sellerId="+sellerId+"&&customerId="+customerId+"&&productId="+productId;
+//        };
+//    }
+//};
 
-
-    let duckTalk = document.querySelector("#duck-talk");
-    let query = duckTalk.dataset.id
-    let sellerId = query.split("/").at(0);
-    let customerId = query.split("/").at(1);
-    let TempProductId = query.split("/").at(2);
-    let productId = Number.parseInt(TempProductId);
-    if(sellerId != "null"){
-        duckTalk.onclick = function (e) {
-            e.preventDefault();
-            console.log("/chatting?sellerId="+sellerId+"&&customerId="+customerId+"&&productId="+productId);
-          window.location.href = "/chatting?sellerId="+sellerId+"&&customerId="+customerId+"&&productId="+productId;
-        };
-    }
-};
-
-	chatting();
+//	chatting();
 
 
 	let imageBox = document.querySelectorAll(".image-box")
