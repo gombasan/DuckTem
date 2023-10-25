a81499ed6c0c3d332c71c8e0df87d0ce68f0f82e
gombasan <xogmlwkdi@naver.com>
2023-10-25 23:41:15 +0900

    feat : 프로젝트 마이그레이션을 위한 서브모듈 추가 - 기존 프로젝트를 스프링 부트 버전과 gradle 로 변경하기 위해 서브모듈을 추가 - 서브모듈로 새로 프로젝트 진행 후 기존 프로젝트 삭제 예정

---
+++ newducktem/src/main/java/otaku/newducktem/NewducktemApplication.java
@@ -0,0 +1,13 @@
+package otaku.newducktem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewducktemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewducktemApplication.class, args);
+	}
+
+}
---
+++ newducktem/src/test/java/otaku/newducktem/NewducktemApplicationTests.java
@@ -0,0 +1,13 @@
+package otaku.newducktem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewducktemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
